371e964eadd9f23bc4ed9e8ba3c233740dddf573
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-12-06 01:47:53 +0100

    add safearea

--- lib/screens/archery/archery.dart
+++ lib/screens/archery/archery.dart
@@ -35,38 +35,40 @@ class _ArcheryScreenState extends State<ArcheryScreen> {
 
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SmartRefresher(
-        controller: refreshController,
-        enablePullDown: true,
-        header: ArcheryHeader(refreshController: refreshController),
-        onRefresh: () async {
-          await Future.delayed(
-              const Duration(seconds: 2),(){
-          });
-          incrementItem();
+      body: SafeArea(
+        child: SmartRefresher(
+          controller: refreshController,
+          enablePullDown: true,
+          header: ArcheryHeader(refreshController: refreshController),
+          onRefresh: () async {
+            await Future.delayed(
+                const Duration(seconds: 2),(){
+            });
+            incrementItem();
 
-          if (!mounted) {
-            return;
-          }
-          refreshController.refreshCompleted();
-        },
-        child: CustomScrollView(
-          slivers: [
-            const SliverAppBar(
-              title: Text('Fitness Targets'),
-              pinned: true,
-            ),
-            SliverList(delegate:
-            SliverChildBuilderDelegate(
-                (context, index){
-                  final reversedDp = dP.reversed.toList();
-                  final dailyProgress = reversedDp[index];
-                  return DailyProgressCard(model: dailyProgress,);
-                },
-                childCount: dP.length,
+            if (!mounted) {
+              return;
+            }
+            refreshController.refreshCompleted();
+          },
+          child: CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                title: Text('Fitness Targets'),
+                pinned: true,
+              ),
+              SliverList(delegate:
+              SliverChildBuilderDelegate(
+                  (context, index){
+                    final reversedDp = dP.reversed.toList();
+                    final dailyProgress = reversedDp[index];
+                    return DailyProgressCard(model: dailyProgress,);
+                  },
+                  childCount: dP.length,
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
